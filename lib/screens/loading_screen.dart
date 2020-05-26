2cc9e17e3d9293671c97f6fff34dc2b49a255532
Deepak Rattan <deepak.rattan@skycap.co.in>
2020-05-26 13:26:30 +0530

    1. Use of http library for hitting apis 2. Json parsing using jsonDecode

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutterweatherapp/services/location.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -37,9 +39,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    // we will wait for the response of get before print statement will work
+    http.Response response = await http.get(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+    if (response.statusCode == 200) {
+      String res = response.body;
+      print('Response Body : $res');
+      // Json parsing
+      var decodeData = jsonDecode(res);
+      var longitude = decodeData['coord']['lon'];
+      print(longitude);
+      var weatherDescription = decodeData['weather'][0]['description'];
+      print('Weather description : $weatherDescription');
+
+      var temp = decodeData['main']['temp'];
+      print('Temp : $temp');
+
+      var id = decodeData['weather'][0]['id'];
+      print('id : $id');
+    } else {
+      print('Response Code :  ${response.statusCode}');
+    }
+  }
+
   // 2 . build method calls every time , our widgets rebuild .
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
